2f2099dbb19e6afed740489a82fdd1233f0756fd
sarah <sarah-holderness@pluralsight.com>
2020-05-26 16:45:48 -0400

    Adding Module 4 solution

--- budget/Expense.py
+++ budget/Expense.py
@@ -41,3 +41,16 @@ class Expenses():
                     unnecessary_expenses2.add(i)
             
             return [necessary_expenses2, food_expenses2, unnecessary_expenses2]
+
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list 
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+                                x.category == 'Classes'  or x.category == 'Utilities' or 
+                                x.category == 'Mortgage'}
+
+        food_expenses = {x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+        
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -5,6 +5,15 @@ def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
+
+    if not divided_set_comp == divided_for_loop:
+        print("Sets are NOT equal by == test")
+
+    for a, b in zip(divided_for_loop, divided_set_comp):
+        if not (a.issubset(b) and 
+            b.issubset(a)):
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
