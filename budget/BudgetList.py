e99f11bb08c301f36ba36b6ae822ed23322d77a7
Will Holderness <Will.Holderness@gmail.com>
2020-05-31 16:15:17 -0400

    Changing to +=

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -11,10 +11,10 @@ class BudgetList:
     def append(self, item):
         if self.sum_expenses + item < self.budget:
             self.expenses.append(item)
-            self.sum_expenses = self.sum_expenses + item
+            self.sum_expenses +=  item
         else:
             self.overages.append(item)
-            self.sum_overages = self.sum_overages + item
+            self.sum_overages += item
 
     def __len__(self):
         return len(self.expenses) + len(self.overages)
