03634caafece041fbf214cd8cd31c22f2a499e24
Will Holderness <Will.Holderness@gmail.com>
2020-05-31 18:27:11 -0400

    Done

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,3 +1,4 @@
+import timeit
 from . import Expense
 import matplotlib.pyplot as plt
 
@@ -11,6 +12,27 @@ def main():
     for a,b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
+    print(timeit.timeit(stmt = "expenses.categorize_for_loop()", setup = 
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''', number=100000, globals=globals()))
+    print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()", setup = 
+    '''
+from . import Expense
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+    ''', number=100000, globals=globals()))
+
+    fig,ax = plt.subplots()
+    labels = ['Necessary', 'Food', 'Unnecessary']
+    divided_expenses_sum = []
+    for category_exps in divided_set_comp:
+        divided_expenses_sum.append( sum( x.amount for x in category_exps))
+    ax.pie( divided_expenses_sum, labels=labels, autopct = '%1.1f%%')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
